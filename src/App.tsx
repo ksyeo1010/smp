00f8e43c59359059347447f80e77083b70ebb791
Kwangsoo <ksyeo1010@gmail.com>
2021-02-20 22:47:29 -0800

    added intial menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
-import './App.global.css';
+import Grid from '@material-ui/core/Grid';
 
-const Hello = () => {
+import './App.global.css';
+import SideBar from './components/SideBar';
+
+const Home = () => {
     return (
         <div>
-            <div className="Hello">
-                <img width="200px" alt="icon" src={icon} />
-            </div>
-            <h1>electron-react-boilerplate</h1>
-            <div className="Hello">
-                <a
-                    href="https://electron-react-boilerplate.js.org/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <button type="button">
-                        <span role="img" aria-label="books">
-                            📚
-                        </span>
-                        Read our docs
-                    </button>
-                </a>
-                <a
-                    href="https://github.com/sponsors/electron-react-boilerplate"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <button type="button">
-                        <span role="img" aria-label="books">
-                            🙏
-                        </span>
-                        Donate
-                    </button>
-                </a>
-            </div>
+            <Grid container>
+                <Grid item xs={4}>
+                    <SideBar />
+                </Grid>
+            </Grid>
         </div>
     );
 };
@@ -44,7 +21,7 @@ export default function App() {
     return (
         <Router>
             <Switch>
-                <Route path="/" component={Hello} />
+                <Route path="/" component={Home} />
             </Switch>
         </Router>
     );
